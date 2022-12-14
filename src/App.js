ae9de84ea54bdbd48a63f07343e26b349e811774
opesnoir <goodsurroundsme@outlook.com>
2022-12-14 15:14:42 +0100

    Routing geimplementeerd en de paths inclusief page not found  gedefinieerd

--- src/App.js
+++ src/App.js
@@ -1,8 +1,27 @@
 import './App.css';
+import {Routes, Route} from "react-router-dom"
+import Home from "./pages/home/Home";
+import NotFound from "./pages/notFound/NotFound";
+import AboutMe from "./pages/aboutMe/AboutMe";
+import Favorites from "./pages/favorites/Favorites";
+import Registration from "./pages/registration/Registration";
+import SearchPageTerm from "./pages/searchPageTerm/SearchPageTerm";
+import SearchPageVerse from "./pages/searchPageVerse/SearchPageVerse";
 
 function App() {
+
   return (
 <>
+
+  <Routes>
+    <Route path="/" element={<Home/>}/>
+    <Route path="/about-me"  element={<AboutMe/>}/>
+    <Route path="/favorites"  element={<Favorites/>}/>
+    <Route path="/registration"  element={<Registration/>}/>
+    <Route path="/search-page-term"  element={<SearchPageTerm/>}/>
+    <Route path="/search-page-verse"  element={<SearchPageVerse/>}/>
+    <Route path="*"  element={<NotFound/>}/>
+  </Routes>
 
   <h1>Hallo</h1>
   <p>Begin hier met coderen!</p>
---
+++ src/pages/searchPageVerse/SearchPageVerse.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const SearchPageVerse = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default SearchPageVerse;
