ac7616d898de3d1d4053cf9355ee192b024173dc
opesnoir <goodsurroundsme@outlook.com>
2022-12-22 10:45:07 +0100

    branche navigate gemerged in branche werk

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import {Routes, Route} from "react-router-dom"
+import {Routes, Route, Navigate} from "react-router-dom"
 import Home from "./pages/home/Home";
 import NotFound from "./pages/notFound/NotFound";
 import AboutMe from "./pages/aboutMe/AboutMe";
@@ -7,17 +7,23 @@ import Favorites from "./pages/favorites/Favorites";
 import Registration from "./pages/registration/Registration";
 import SearchPageTerm from "./pages/searchPageTerm/SearchPageTerm";
 import SearchPageVerse from "./pages/searchPageVerse/SearchPageVerse";
+import Navigation from "./components/navigation/Navigation"
+import ProfilePage from "./pages/profilePage/ProfilePage";
 
 function App() {
+
+  const isLoggedin= false;
 
   return (
 <>
 
+  <Navigation/>
   <Routes>
     <Route path="/" element={<Home/>}/>
     <Route path="/about-me"  element={<AboutMe/>}/>
-    <Route path="/favorites"  element={<Favorites/>}/>
+    <Route path="/favorites" element={isLoggedin === true ? <Favorites/> : <Navigate to="/"/>}/>
     <Route path="/registration"  element={<Registration/>}/>
+    <Route path="/profile-page" element={isLoggedin === true ? <ProfilePage/> : <Navigate to="/"/>}/>
     <Route path="/search-page-term"  element={<SearchPageTerm/>}/>
     <Route path="/search-page-verse"  element={<SearchPageVerse/>}/>
     <Route path="*"  element={<NotFound/>}/>
