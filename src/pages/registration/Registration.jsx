554e0f85edfd29f93744a304157cd2a1f3732330
opesnoir <goodsurroundsme@outlook.com>
2022-12-27 15:02:24 +0100

    naam en email errors af

--- src/pages/registration/Registration.jsx
+++ src/pages/registration/Registration.jsx
@@ -3,73 +3,13 @@ import styles from "./Registration.module.css";
 import imageSheep from "../../assets/register-sheep-pexels-trinity-kubassek-288621.jpg";
 import {useForm} from "react-hook-form";
 
-/*function App() {
-    const methods = useForm();
 
-    return null;
-}*/
-
-/*deze methode werkt: {errors?.name?.message}
-deze ook: {errors[name]}*/
-
-/*<div>
-    <form onSubmit={handleSubmit(onSubmit)}>
-        <div>
-            <label htmlFor="name-field">Naam:</label>
-            <input
-                type="text"
-                id="name-field"
-                placeholder="Naam"
-                {...register("name",{
-                    required:{
-                        value:true,
-                        message: "Dit veld is verplicht",
-                    }
-                })}/>
-            <label htmlFor="email-field">Email:</label>
-            <input
-                type="text"
-                id="email-field"
-                placeholder="Email"
-                {...register("email",{
-                    required:{
-                        value:true,
-                        message:"Dit veld is verplicht",
-                    },
-                    validate:(value)=> value.includes("@") || "Het emailadres moet een @ bevatten",
-                })}/>
-            <label htmlFor="password-field">Wachtwoord:</label>
-            <input
-                type="text"
-                id="password-field"
-                placeholder="Wachtwoord"
-                {...register("password", {
-                    required:{
-                        value:true,
-                        message: "Het wachtwoord bevat minimaal 8 tekens, waarvan 1 hoofdletter en 1 leesteken.",
-                    },
-                    pattern:{
-                        value: /^(?=.*[0-9])(?=.*[a-zA-Z])([a-zA-Z0-9]+)$/,
-                        message: "Het wachtwoord bevat minimaal 1 hoofdletter en 1 leesteken",
-                    },
-                    minLength: {
-                        value: 8,
-                        message: "Het wachtwoord bevat minimaal 8 tekens",
-                    },
-                })}/>
-        </div>
-
-        <div>
-            <button type="submit">Registreren</button>
-        </div>
-    </form>
-</div>*/
 
 
 
 const Registration = () => {
 
-    const {handleSubmit, register, formState:{errors}  } = useForm();
+    const {handleSubmit, register, formState:{errors} } = useForm();
 
     const onSubmit = (data) => {
         console.log(data);
@@ -89,7 +29,7 @@ const Registration = () => {
                 </div>
             </div>
 
-            <form className="registration-form-container" onSubmit={handleSubmit(onSubmit)}>
+            <form className="registration-form-container" onSubmit={handleSubmit(onSubmit)} noValidate>
                 <label htmlFor="name-field">
                     <input
                         type="text"
@@ -99,11 +39,11 @@ const Registration = () => {
                         {...register("name",{
                             required:{
                                 value: true,
-                                message: "Naam is verplicht"
+                                message: "Gebruikersnaam is verplicht"
                             }
                         })}
                     />
-                    {errors.name && <p>Gebruikersnaam is verplicht</p>}
+                    {errors.name && <p>{errors.name.message}</p>}
                 </label>
                 <label htmlFor="email-field">
                 <input
@@ -115,11 +55,15 @@ const Registration = () => {
                             required: {
                                 value: true,
                                 message: "Dit veld is verplicht",
+                            },
+                            pattern: {
+                                value: /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+                                message: "Gebruik een geldig emailadres"
                             }
                         }
                     )}
                 />
-                    {errors.email && <p>Geldig emailadres is verplicht</p>}
+                    {errors.email && <p>{errors.email.message}</p>}
             </label>
                 <label htmlFor="password-field">
                     <input
@@ -127,8 +71,23 @@ const Registration = () => {
                         name="password"
                         id="password-field"
                         placeholder="Wachtwoord"
+                        {...register("password",{
+                            required:{
+                                value: true,
+                                message: "Dit veld is verplicht"
+                            },
+                            minLength: {
+                                value: 8,
+                                message: "Het wachtwoord bevat minimaal 8 tekens",
+                            },
+                            pattern:{
+                                value: ,
+                                message: "Het wachtwoord bevat minimaal 1 hoofdletter en 1 leesteken",
+                            },
+                        })}
                     />
                 </label>
+                {errors.password && <p> {errors.password.message} </p>}
                 <button type="submit">Registreren</button>
 
             </form>
