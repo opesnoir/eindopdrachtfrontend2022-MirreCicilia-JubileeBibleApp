9024c749e5555230f6106046b6ba6792be22ee5a
opesnoir <goodsurroundsme@outlook.com>
2022-12-27 13:16:43 +0100

    registratieformulier naam en email voorzien van validatie

--- src/pages/registration/Registration.jsx
+++ src/pages/registration/Registration.jsx
@@ -12,10 +12,66 @@ import {useForm} from "react-hook-form";
 /*deze methode werkt: {errors?.name?.message}
 deze ook: {errors[name]}*/
 
+/*<div>
+    <form onSubmit={handleSubmit(onSubmit)}>
+        <div>
+            <label htmlFor="name-field">Naam:</label>
+            <input
+                type="text"
+                id="name-field"
+                placeholder="Naam"
+                {...register("name",{
+                    required:{
+                        value:true,
+                        message: "Dit veld is verplicht",
+                    }
+                })}/>
+            <label htmlFor="email-field">Email:</label>
+            <input
+                type="text"
+                id="email-field"
+                placeholder="Email"
+                {...register("email",{
+                    required:{
+                        value:true,
+                        message:"Dit veld is verplicht",
+                    },
+                    validate:(value)=> value.includes("@") || "Het emailadres moet een @ bevatten",
+                })}/>
+            <label htmlFor="password-field">Wachtwoord:</label>
+            <input
+                type="text"
+                id="password-field"
+                placeholder="Wachtwoord"
+                {...register("password", {
+                    required:{
+                        value:true,
+                        message: "Het wachtwoord bevat minimaal 8 tekens, waarvan 1 hoofdletter en 1 leesteken.",
+                    },
+                    pattern:{
+                        value: /^(?=.*[0-9])(?=.*[a-zA-Z])([a-zA-Z0-9]+)$/,
+                        message: "Het wachtwoord bevat minimaal 1 hoofdletter en 1 leesteken",
+                    },
+                    minLength: {
+                        value: 8,
+                        message: "Het wachtwoord bevat minimaal 8 tekens",
+                    },
+                })}/>
+        </div>
+
+        <div>
+            <button type="submit">Registreren</button>
+        </div>
+    </form>
+</div>*/
+
+
+
 const Registration = () => {
 
-    const {handleSubmit, formState:{errors}, register} = useForm();
-    const onSubmit =(data) =>{
+    const {handleSubmit, register, formState:{errors}  } = useForm();
+
+    const onSubmit = (data) => {
         console.log(data);
     };
 
@@ -33,62 +89,50 @@ const Registration = () => {
                 </div>
             </div>
 
-            <div>
-                <form onSubmit={handleSubmit(onSubmit)}>
-                    <div>
-                        <label htmlFor="name-field">Naam:</label>
-                        <input
-                            type="text"
-                            id="name-field"
-                            placeholder="Naam"
-                            {...register("name",{
-                                required:{
-                                    value:true,
-                                    message: "Dit veld is verplicht"
-                                }
-                            })}/>
-                        <label htmlFor="email-field">Email:</label>
-                        <input
-                            type="text"
-                            id="email-field"
-                            placeholder="Email"
-                            {...register("email",{
-                                required:{
-                                    value:true,
-                                    message:"Dit veld is verplicht",
-                                },
-                                validate:(value)=> value.includes("@") || "Het emailadres moet een @ bevatten"
-                            })}/>
-                        <label htmlFor="password-field">Wachtwoord:</label>
-                        <input
-                            type="text"
-                            id="password-field"
-                            placeholder="Wachtwoord"
-                            {...register("password", {
-                                required:{
-                                    value:true,
-                                    message: "Het wachtwoord bevat minimaal 8 tekens, waarvan 1 hoofdletter en 1 leesteken.",
-                                },
-                                pattern:{
-                                    value: /^(?=.*[0-9])(?=.*[a-zA-Z])([a-zA-Z0-9]+)$/,
-                                    message: "Het wachtwoord bevat minimaal 1 hoofdletter en 1 leesteken",
-                                },
-                                minLength: {
-                                    value: 8,
-                                    message: "Het wachtwoord bevat minimaal 8 tekens",
-                                },
-                            })}/>
-                        {errors?.name?.message}
-                        {errors[name]}
-                    </div>
-                    <div>
-                        <p>Het wachtwoord bevat minimaal 8 tekens, waarvan 1 hoofdletter en 1 leesteken.</p>
-                    </div>
-                    <div>
-                        <button type="submit">Registreren</button>
-                    </div>
-                </form>
-            </div>
+            <form className="registration-form-container" onSubmit={handleSubmit(onSubmit)}>
+                <label htmlFor="name-field">
+                    <input
+                        type="text"
+                        name="name"
+                        id="name-field"
+                        placeholder="Naam:"
+                        {...register("name",{
+                            required:{
+                                value: true,
+                                message: "Naam is verplicht"
+                            }
+                        })}
+                    />
+                    {errors.name && <p>Gebruikersnaam is verplicht</p>}
+                </label>
+                <label htmlFor="email-field">
+                <input
+                    type="email"
+                    name="email"
+                    id="email-field"
+                    placeholder="Email:"
+                    {...register("email", {
+                            required: {
+                                value: true,
+                                message: "Dit veld is verplicht",
+                            }
+                        }
+                    )}
+                />
+                    {errors.email && <p>Geldig emailadres is verplicht</p>}
+            </label>
+                <label htmlFor="password-field">
+                    <input
+                        type="password"
+                        name="password"
+                        id="password-field"
+                        placeholder="Wachtwoord"
+                    />
+                </label>
+                <button type="submit">Registreren</button>
+
+            </form>
+
         </>
     );
 };
