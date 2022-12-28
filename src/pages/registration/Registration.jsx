1d103f2aee44f86bbd2e5617f4ec1832b3aa7fa8
opesnoir <goodsurroundsme@outlook.com>
2022-12-28 16:30:37 +0100

    registratieformulier herhaal wachtwoord en error messages toegevoegd

--- src/pages/registration/Registration.jsx
+++ src/pages/registration/Registration.jsx
@@ -1,19 +1,21 @@
-import React, {useState} from "react";
+import React, {useRef, useState} from "react";
 import styles from "./Registration.module.css";
 import imageSheep from "../../assets/register-sheep-pexels-trinity-kubassek-288621.jpg";
 import {useForm} from "react-hook-form";
-import PasswordChecklist from "react-password-checklist";
 
 
 const Registration = () => {
 
-    const {handleSubmit, register, formState: {errors}} = useForm();
-    const [password, setPassword] = useState(" ");
-    const [confirmPassword, setConfirmPassword] = useState(" ")
+    const {handleSubmit, register, formState: {errors}, watch} = useForm();
+    const password = useRef({});
+    password.current = watch("password", "");
+    /*const [password, setPassword] = useState(" ");
+    const [confirmPassword, setConfirmPassword] = useState(" ")*/
 
     const onSubmit = (data) => {
         console.log(data);
     };
+
 
     return (
         <>
@@ -33,76 +35,90 @@ const Registration = () => {
                         </div>
 
                         <div className={styles.registrationFormContainer}>
-                        <form className={styles.registrationFormContent} onSubmit={handleSubmit(onSubmit)} noValidate>
-                            <label className={styles.registrationFormItem} htmlFor="name-field">
-                                <input
-                                    type="text"
-                                    name="name"
-                                    id="name-field"
-                                    placeholder="Naam:"
-                                    {...register("name", {
-                                        required: {
-                                            value: true,
-                                            message: "Gebruikersnaam is verplicht"
-                                        }
-                                    })}
-                                />
-                                {errors.name && <p>{errors.name.message}</p>}
-                            </label>
-                            <label className={styles.registrationFormItem} htmlFor="email-field">
-                                <input
-                                    type="email"
-                                    name="email"
-                                    id="email-field"
-                                    placeholder="Email:"
-                                    {...register("email", {
+                            <form className={styles.registrationFormContent} onSubmit={handleSubmit(onSubmit)}
+                                  noValidate>
+                                <label className={styles.registrationFormItem} htmlFor="name-field">
+                                    <input
+                                        type="text"
+                                        name="name"
+                                        id="name-field"
+                                        placeholder="Naam:"
+                                        {...register("name", {
                                             required: {
                                                 value: true,
-                                                message: "Dit veld is verplicht",
-                                            },
-                                            pattern: {
-                                                value: /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
-                                                message: "Gebruik een geldig emailadres"
+                                                message: "Gebruikersnaam is verplicht"
                                             }
-                                        }
-                                    )}
-                                />
-                                {errors.email && <p>{errors.email.message}</p>}
-                            </label>
-                            <label className={styles.registrationFormItem} htmlFor="password-field">
-                                <input
-                                    type="password"
-                                    name="password"
-                                    id="password-field"
-                                    placeholder="Wachtwoord:"
-                                    onChange={e => setPassword(e.target.value)}
-                                /></label>
-                            <label className={styles.registrationFormItem} htmlFor="confirm-password-field">
-                                <input
-                                    type="password"
-                                    name="confirmpassword"
-                                    id="password-field"
-                                    placeholder="Bevestig wachtwoord:"
-                                    onChange={e => setConfirmPassword(e.target.value)}
-                                /></label>
+                                        })}
+                                    />
+                                    {errors.name && <p>{errors.name.message}</p>}
+                                </label>
+                                <label className={styles.registrationFormItem} htmlFor="email-field">
+                                    <input
+                                        type="email"
+                                        name="email"
+                                        id="email-field"
+                                        placeholder="Email:"
+                                        {...register("email", {
+                                                required: {
+                                                    value: true,
+                                                    message: "Dit veld is verplicht",
+                                                },
+                                                pattern: {
+                                                    value: /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+                                                    message: "Gebruik een geldig emailadres"
+                                                }
+                                            }
+                                        )}
+                                    />
+                                    {errors.email && <p>{errors.email.message}</p>}
+                                </label>
+                                <label className={styles.registrationFormItem} htmlFor="password-field">
+                                    <input
+                                        type="password"
+                                        name="password"
+                                        id="password-field"
+                                        placeholder="Wachtwoord:"
+                                        /*onChange={e => setPassword(e.target.value)}*/
+                                        {...register("password", {
+                                                required: {
+                                                    value: true,
+                                                    message: "Dit veld is verplicht",
+                                                },
+                                                pattern: {
+                                                    value: /^(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/,
+                                                    message: "Het wachtwoord bevat minimaal 8 tekens, waarvan 1 hoofdletter, 1 cijfer en 1 leesteken.",
+                                                }
+                                            }
+                                        )}
+                                    />
+                                    {errors.password && <p>{errors.password.message}</p>}
+                                </label>
+                                <label className={styles.registrationFormItem} htmlFor="confirmpassword">
+                                    <input
+                                        type="password"
+                                        name="confirmpassword"
+                                        id="password-field-confirm"
+                                        placeholder="Herhaal wachtwoord"
+                                        /*onChange={}*/
+                                        {...register("confirmpasword",{
+                                            required: {
+                                                value:true,
+                                            },
+                                            validate: value =>
+                                                value === password.current, /*|| "De wachtwoorden komen niet overeen",*/
+                                                message: "De wachtwoorden komen niet overeen",
+                                            })}
+                                    />
+                                    {errors.confirmpassword && <p>{errors.confirmpassword.message}</p>
+                                    }
 
-                            <div className={styles.registrationItemPasswordChecklistContainer}>
-                                <PasswordChecklist className={styles.registrationItemPasswordChecklistContent}
-                                                   rules={["minLength", "specialChar", "number"]}
-                                                   minLength={8}
-                                                   value={password}
-                                                   valueAgain={confirmPassword}
-                                                   messages={{
-                                                       minLength: "karakters",
-                                                       specialChar: "leesteken",
-                                                       number: "cijfer",
-                                                   }}
-                                />
-                            </div>
-                            <div className={styles.registrationItemButtonContainer}>
-                                <button className={styles.registrationItemButton} type="submit">Registreren</button>
-                            </div>
-                        </form>
+
+                                </label>
+
+                                <div className={styles.registrationItemButtonContainer}>
+                                    <button className={styles.registrationItemButton} type="submit">Registreren</button>
+                                </div>
+                            </form>
                         </div>
                     </div>
                 </div>
